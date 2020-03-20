cfe18a88637610b201d4484f020a2ed7f7c8a93d
Gabriel Fernella Garcia dos Santos <Gabrielfernella@users.noreply.github.com>
2020-03-20 08:51:24 -0300

    Initial Config and structure
    First config: webpack, babel and initial structure aplication ReactJS.

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    //configurações do babel
+    presets: [
+        "@babel/preset-env", //responsavel por alterar as funcionalidades do javascript para que o navegador entenda
+        "@babel/preset-react" //transformar componentes que o navegador não entende do react
+    ],
+    
+};
+
+//padraõ em todo proj.
